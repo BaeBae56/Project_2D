f16f6b22772fdfc0129b84190adb2046a1cea434
Peachzy <thanapong.yamn@bumail.net>
2025-04-29 00:24:16 +0700

    Animator_2D
    ขยับตามแล้ว

--- Assets/Character2D.cs
+++ Assets/Character2D.cs
@@ -9,9 +9,11 @@ public class Character2D : MonoBehaviour
     public float JumpForce = 1;
 
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,15 +21,17 @@ public class Character2D : MonoBehaviour
     {
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
-        
+        _animator.SetFloat("Speed", Mathf.Abs(movement));
+
         if (movement > 0.01f)
         {
             transform.localScale = Vector3.one;
-
+            
         }
         else if (movement < -0.01f)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+            
 
         }
         else if (movement == 0f)
@@ -38,6 +42,20 @@ public class Character2D : MonoBehaviour
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f ) 
         { 
             _rigidbody.AddForce(new Vector2(0 , JumpForce), ForceMode2D.Impulse);
+            _animator.SetTrigger("Jump");
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
+
+        {
+            _animator.SetBool("Slide", true);
+            MovementSpeed = 11;
+        }
+        else 
+        {
+            _animator.SetBool("Slide", false);
+            MovementSpeed = 8;
         }
     }
 }
